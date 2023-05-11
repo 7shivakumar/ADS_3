70e774958c4ed06397df0b7d0b45920ff224d31c
7shivakumar <126573994+7shivakumar@users.noreply.github.com>
2023-05-11 23:45:45 +0000

    Add files via upload

--- 22024927.py
+++ 22024927.py
@@ -12,6 +12,61 @@ import matplotlib.pyplot as plt
 import scipy.optimize as opt
 from sklearn.cluster import KMeans
 
+def converttonumbers(df, columnlist):
+    df = df.replace(to_replace="..", value="0")
+    df[columnlist] = df[columnlist].apply(pd.to_numeric)
+    return df
+
+def equation(x, a, b, c):
+    '''
+    This is the function which returns the quadratic function.
+    Quadratic equation used for curve fitting.
+
+    Args:
+        x (numpy.ndarray): Input array.
+        a (float): Coefficient of x^2 term.
+        b (float): Coefficient of x term.
+        c (float): Constant term.
+
+    Returns:
+        numpy.ndarray: Result of the quadratic equation.
+    '''
+    return a * x**2 + b * x + c
+
+def curveFit(df_CO2, df_elec, country_code, years):
+    '''
+    Perform curve fitting and plot the curve.
+
+    Args:
+        df_CO2 (pandas.DataFrame): DataFrame containing CO2 emission data.
+        df_elec (pandas.DataFrame): DataFrame containing electricity production data.
+        country_code (str): Country code for the specific country.
+        years (list): List of years to consider for curve fitting.
+
+    Returns:
+        None
+    '''
+    df_CO2 = df_CO2.replace(to_replace="..", value="0")
+    df_CO2 = df_CO2.loc[df_CO2['Country Code'] == country_code][years]
+    df_CO2 = df_CO2.dropna(axis=1)
+    
+    df_elec = df_elec.replace(to_replace="..", value="0")
+    df_elec = df_elec.loc[df_elec['Country Code'] == country_code][years]
+    df_elec = df_elec.dropna(axis=1)
+    
+    years = df_CO2.columns[4:].tolist()
+    
+    x, _ = opt.curve_fit(equation, df_CO2[years].values.flatten(), df_elec[years].values.flatten())
+    
+    df_elec[years] = equation(df_CO2[years].values.flatten(), *x)
+    
+    plt.figure()
+    plt.plot(df_CO2[years].values.flatten(), df_elec[years].values.flatten(), color='green')
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.legend(["Expected"], loc="upper right")
+    plt.title("Curve Fit of " + country_code)
+    plt.show()
 
 data_CO2 = pd.read_csv("CO2Emission.csv", skiprows=4)
 df_CO2 = pd.DataFrame(data_CO2)
